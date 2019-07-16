4dacdadc2d4e3862d427f8288ccdc1dbf519ccc5
githubyosshi <kanrisya.shisutemu@gmail.com>
2019-07-16 12:09:13 +0900

    variable commit

--- myapp.kt
+++ myapp.kt
@@ -1,8 +1,19 @@
 // comento
 /* comento
 coment
-coment
  */
+// fun main(args: Array<String>) {
+//   println("Hello World")
+// }
+
+// 変数
+// - val  再代入できない
+// - var  再代入できる
 fun main(args: Array<String>) {
-  println("Hello World")
+  // val msg: String = "Hello World"
+  // val msg = "Hello World" // 型推論
+  var msg = "Hello World"
+  println(msg)
+  msg = "Hello Word 2"
+  println(msg)
 }
