784e5e58127f705528b5fcfe18e4ee965c7ad927
githubyosshi <kanrisya.shisutemu@gmail.com>
2019-07-16 14:41:09 +0900

    Basic data type commit

--- myapp.kt
+++ myapp.kt
@@ -12,8 +12,23 @@ coment
 fun main(args: Array<String>) {
   // val msg: String = "Hello World"
   // val msg = "Hello World" // 型推論
-  var msg = "Hello World"
-  println(msg)
-  msg = "Hello Word 2"
-  println(msg)
+  // var msg = "Hello World"
+  // println(msg)
+  // msg = "Hello Word 2"
+  // println(msg)
+
+  // String, Char
+  val msg: String = "Hello World"
+  val c: Char = 'a'
+
+  // Byte, Short, Int, Long
+  val i: Int = 100
+  val i: Long = 5555555555555L
+
+  // Float, Double
+  val d: Double = 234.523
+  val f: Float = 12.344F
+
+  // Boolean (true / false)
+  val flag: Boolean = true
 }
